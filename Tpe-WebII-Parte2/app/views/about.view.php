630b487039805b5fa94ce244c2277494e70b2f18
santicarrillo <128561463+santicarrillo@users.noreply.github.com>
2023-10-12 13:36:12 -0600

    Delete Tpe-WebII-Parte2 directory

--- Tpe-WebII-Parte2/app/views/about.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AboutView {
-    public function showAbout() {
-        require 'templates/header.php';
-        echo "aca va la info de about";
-        require 'templates/footer.php';
-    }
-}
